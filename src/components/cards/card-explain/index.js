568c0d3f0e01d4366741232954d84d965dd9aaae
NFADLIM <hamdibgr@gmail.com>
2023-06-23 05:24:36 +0700

    update

--- src/components/cards/card-explain/index.js
+++ src/components/cards/card-explain/index.js
@@ -19,30 +19,19 @@ const ExplainCard = () => {
 				style={{
 					width: '55rem',
 					border: '3px solid #E4372A',
-					backgroundImage: `url(${bg})`,
-					backgroundRepeat: 'no-repeat',
-					backgroundPosition: 'center',
-					backgroundSize: 'cover',
 					borderRadius: '20px',
 				}}
 			>
 				<Card.Body
 					style={{
-						color: 'white',
 						textAlign: 'left',
 					}}
 				>
 					<p>
-						Selamat datang di Universitas Pertamina. Kamu dapat menggunakan
-						mouse atau langsung menggunakan tangan. Geser layar untuk melihat
-						sekitar. Gunakan klik kiri mouse untuk melihat-lihat. Tekan secara
-						bersamaan untuk memperbesar dan memperkecil. <br />
-						Universitas Pertamina merupakan kampus swasta non-kedinasan yang
-						berada di bawah naungan Yayasan Pertamina. Universitas Pertamina
-						didirikan pada tahun 2016. Universitas Pertamina saat ini memiliki 6
-						Fakultas dan 15 Program Studi strata 1 dan berlokasi di Jl. Teuku
-						Nyak Arief, RT.7/RW.8, Simprug, Kec. Kby. Lama, Kota Jakarta
-						Selatan.
+						Selamat datang di Virtual Tour Universitas Pertamina. Anda dapat menggunakan
+						mouse atau langsung menggunakan tangan untuk mengendalikan virtual tour. 
+						Penjelasan pengendalian lebih jelasnya, berada di bawah virtual tour. <br />
+						.
 					</p>
 					<p></p>
 				</Card.Body>
